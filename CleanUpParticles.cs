faa8d6c1a06c0a26c63d7cfe17a5cc7a2275c5d6
Chad Keating <deadlysin4@gmail.com>
2015-01-31 21:07:42 +0000

    Added CleanUpParticles
    Self Destruction for one shot particle systems

---
+++ CleanUpParticles.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+//Self Destruction for one shot particle systems
+public class CleanUpParticles : MonoBehaviour {
+	ParticleSystem  oneShotSystem;
+	void Start () {
+		oneShotSystem = GetComponent<ParticleSystem>();
+	}
+	void Update () {
+		if(oneShotSystem)
+		{
+			if(!oneShotSystem.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
