659437c604cdf42a5a921a521c3801306cc40c8d
Chad Keating <deadlysin4@gmail.com>
2015-02-10 19:23:39 +0000

    Updated RaycastTarget

--- Misc/RaycastTarget.cs
+++ Misc/RaycastTarget.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public static class RaycastTarget {
 
@@ -10,14 +9,18 @@ public static class RaycastTarget {
 
 	public static Vector3 Cast(Vector3 screenPoint)
 	{
+		return Cast(screenPoint, Vector3.up);
+	}
+
+	public static Vector3 Cast(Vector3 screenPoint, Vector3 axis)
+	{
 		Ray ray = Camera.main.ScreenPointToRay(screenPoint);
-		Plane basePlane = new Plane(Vector3.back, Vector3.zero);
+		Plane basePlane = new Plane(axis, Vector3.zero);
 		float rayDistance;
 		if (!basePlane.Raycast(ray, out rayDistance)) {
 			Debug.LogError("Nothing was hit.");
 			return new Vector3(0,0,0); //We hit nothing??
 		};
-
 		return ray.GetPoint(rayDistance);
 	}
 
