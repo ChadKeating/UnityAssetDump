31cadb385e6bb390b7d6b797c8f7e18ac4cdeed8
Chad Keating <deadlysin4@gmail.com>
2015-02-15 03:19:24 +0000

    Added TileHelper

---
+++ Misc/TileHelper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileHelper : MonoBehaviour {
+
+	public Material positive;
+	public Material negative;
+	public int colliders = 0;
+
+	void OnTriggerExit(Collider other)
+	{
+		colliders--;
+		if (colliders == 0)
+		{
+			this.renderer.material = positive;
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (colliders == 0 && other.gameObject.tag == "Building")
+		{
+			this.renderer.material = negative;
+		}
+		colliders++;
+	}
+
+}
